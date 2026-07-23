518284bd11843acdc595a0cb4b26b3707e701777
Ramiro Najera <joseramironajeraenriquez@gmail.com>
2026-07-23 16:14:46 +0200

    Added Write and Read Register functions, ConfigureChannel

--- plib_ad4115.c
+++ plib_ad4115.c
@@ -1,2 +1,216 @@
 #include "plib_ad4115.h"
 
+#define WEN_READ_CMD_MASK       0b01000000
+
+// todo: delete after implementing use of conf struct
+#define CH_EN_MASK              0x8000
+
+typedef enum
+{
+    STATUS_REG              = 0b000000,
+    ADC_MODE_REG            = 0b000001,
+    INTERFACE_MODE_REG      = 0b000010,
+    REGISTER_CHECKSUM_REG   = 0b000011,
+    DATA_REG                = 0b000100,
+    GPIO_CONFIG_REG         = 0b000110,
+    ID_REG                  = 0b000111,
+
+    CHANNEL0_REG            = 0b010000,
+    CHANNEL1_REG            = 0b010001,
+    CHANNEL2_REG            = 0b010010,
+    CHANNEL3_REG            = 0b010011,
+    CHANNEL4_REG            = 0b010100,
+    CHANNEL5_REG            = 0b010101,
+    CHANNEL6_REG            = 0b010110,
+    CHANNEL7_REG            = 0b010111,
+    CHANNEL8_REG            = 0b011000,
+    CHANNEL9_REG            = 0b011001,
+    CHANNEL10_REG           = 0b011010,
+    CHANNEL11_REG           = 0b011011,
+    CHANNEL12_REG           = 0b011100,
+    CHANNEL13_REG           = 0b011101,
+    CHANNEL14_REG           = 0b011110,
+    CHANNEL15_REG           = 0b011111,
+
+    SETUP_CONFIG0_REG       = 0b100000,
+    SETUP_CONFIG1_REG       = 0b100001,
+    SETUP_CONFIG2_REG       = 0b100010,
+    SETUP_CONFIG3_REG       = 0b100011,
+    SETUP_CONFIG4_REG       = 0b100100,
+    SETUP_CONFIG5_REG       = 0b100101,
+    SETUP_CONFIG6_REG       = 0b100110,
+    SETUP_CONFIG7_REG       = 0b100111,
+
+    FILTER_CONFIG0_REG      = 0b101000,
+    FILTER_CONFIG1_REG      = 0b101001,
+    FILTER_CONFIG2_REG      = 0b101010,
+    FILTER_CONFIG3_REG      = 0b101011,
+    FILTER_CONFIG4_REG      = 0b101100,
+    FILTER_CONFIG5_REG      = 0b101101,
+    FILTER_CONFIG6_REG      = 0b101110,
+    FILTER_CONFIG7_REG      = 0b101111,
+
+    OFFSET0_REG             = 0b110000,
+    OFFSET1_REG             = 0b110001,
+    OFFSET2_REG             = 0b110010,
+    OFFSET3_REG             = 0b110011,
+    OFFSET4_REG             = 0b110100,
+    OFFSET5_REG             = 0b110101,
+    OFFSET6_REG             = 0b110110,
+    OFFSET7_REG             = 0b110111,
+
+    GAIN0_REG               = 0b111000,
+    GAIN1_REG               = 0b111001,
+    GAIN2_REG               = 0b111010,
+    GAIN3_REG               = 0b111011,
+    GAIN4_REG               = 0b111100,
+    GAIN5_REG               = 0b111101,
+    GAIN6_REG               = 0b111110,
+    GAIN7_REG               = 0b111111,
+
+} AD4115_Register_t;
+
+/*==============================================================================
+ * Private function prototypes
+ *============================================================================*/
+
+static void Write(SPI_t *spi, uint8_t* data, const uint16_t size);
+static void Read(SPI_t *spi, uint8_t* data, const uint16_t size);
+static void StartTranmission(SPI_t *spi);
+static void EndTramission(SPI_t *spi);
+static void ReadRegister(SPI_t *spi, uint8_t reg, uint8_t *data, uint8_t len);
+static void WriteRegister(SPI_t *spi, uint8_t reg, uint8_t *data, uint8_t len);
+static void Reset(AD4115_t *obj);
+static void ConfigureChannel(AD4115_t *obj, uint8_t channel, uint8_t setup, uint16_t input);
+static void ReadId(AD4115_t *obj, uint8_t *data);
+static void ConfigureSetup(AD4115_t *obj, uint16_t conf);
+/*==============================================================================
+ * Private data
+ *============================================================================*/
+
+/*==============================================================================
+ * Public functions
+ *============================================================================*/
+
+uint8_t AD4115_Init(AD4115_t *obj)
+{
+    // Reset and configure AD4115
+    Reset(obj);
+
+    uint8_t check = AD4115_CheckConnection(obj);
+
+    // channel configuration (select input and setup for each adc channel)
+    // todo: modify with modular function (this is just for testing)
+    ConfigureChannel(obj, 0, 0, INPUT_VIN0_VINCOM);
+    ConfigureChannel(obj, 1, 0, INPUT_VIN0_VINCOM);
+    ConfigureChannel(obj, 2, 0, INPUT_VIN0_VINCOM);
+    ConfigureChannel(obj, 3, 0, INPUT_VIN0_VINCOM);
+    
+    // setup configuration (8 possible adc setups, filter order, output data rate, and more)
+    ConfigureSetup(obj, 0);     //todo: continue developping this function
+    // adc mode and interface mode configuration (adc mode, clock source, enable crc, data and status and more)
+
+    return check;
+}
+
+uint8_t AD4115_CheckConnection(AD4115_t *obj)
+{
+    uint8_t id[2];
+    ReadId(obj, id);
+    if(id[0] == 0x34)
+        return 1;
+    return 0;
+}
+
+ /*==============================================================================
+ * Private functions
+ *============================================================================*/
+
+static void Write(SPI_t *spi, uint8_t* data, const uint16_t size)
+{
+    if(spi->Write)
+        spi->Write(data, size);
+}
+
+static void Read(SPI_t *spi, uint8_t* data, const uint16_t size)
+{
+    if(spi->Read)
+        spi->Read(data, size);
+}
+
+static void StartTranmission(SPI_t *spi)
+{
+    if(spi->pinEN.Clear != NULL)
+        spi->pinEN.Clear();
+    if(spi->pinCS.Clear != NULL) 
+        spi->pinCS.Clear();
+}
+
+static void EndTramission(SPI_t *spi)
+{
+    if(spi->pinCS.Set != NULL)
+        spi->pinCS.Set();
+    if(spi->pinEN.Set != NULL)
+        spi->pinEN.Set();
+}
+
+static void ReadRegister(SPI_t *spi, uint8_t reg, uint8_t *data, uint8_t len)
+{
+    // Write the register address
+    // bit 7 !WEN must be 0, bit 6 R/W must be 1 to read, bit 5..0 register address
+    StartTranmission(spi);
+    uint8_t addr = (WEN_READ_CMD_MASK) | (reg & 0x3F);
+    Write(spi, &addr, 1);
+    // Read data
+    Read(spi, data, len);
+    EndTramission(spi);
+}
+
+static void WriteRegister(SPI_t *spi, uint8_t reg, uint8_t *data, uint8_t len)
+{
+    StartTranmission(spi);
+    uint8_t addr = (reg & 0x3F);
+    Write(spi, &addr, 1);
+    Write(spi, data, len);
+    // Write CRC ?? 
+    EndTramission(spi);
+}
+
+static void Reset(AD4115_t *obj)
+{
+
+    StartTranmission(&obj->spi);
+    uint8_t buffer[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    Write(&obj->spi, buffer, 8);
+    EndTramission(&obj->spi);
+}
+
+static void ConfigureChannel(AD4115_t *obj, uint8_t channel, uint8_t setup, uint16_t input)
+{
+    uint16_t ch_cfg = CH_EN_MASK | (((uint16_t)(setup & 0x07)) << 12) | (input & 0x03FF);
+
+    uint8_t data[2] =
+    {
+        (uint8_t)(ch_cfg >> 8),
+        (uint8_t)(ch_cfg)
+    };
+
+    WriteRegister(&obj->spi,
+                  (uint8_t)(CHANNEL0_REG + channel),
+                  data,
+                  sizeof(data));
+}
+
+static void ReadId(AD4115_t *obj, uint8_t *data)
+{
+    ReadRegister(&obj->spi, ID_REG, data, 2);
+}
+
+static void ConfigureSetup(AD4115_t *obj, uint16_t conf)
+{
+    /*
+    WriteRegister(&obj->spi, SETUP_CONFIG0_REG, )
+    if (setup_number > 7) { return; }
+	write_register(Register::SETUP0 + setup_number, setup_bits);
+    */
+}
--- plib_ad4115.h
+++ plib_ad4115.h
@@ -9,7 +9,125 @@
 #define PLIB_AD4115_H
 
 #include <stdint.h>
+#include "libs/common_c_libs/plib_comm_struct.h"
+
+typedef enum
+{
+    INPUT_VIN0_VIN1         = 0b0000000001,
+    INPUT_VIN0_VINCOM       = 0b0000010000,
+
+    INPUT_VIN1_VIN0         = 0b0000100000,
+    INPUT_VIN1_VINCOM       = 0b0000110000,
+
+    INPUT_VIN2_VIN3         = 0b0001000011,
+    INPUT_VIN2_VINCOM       = 0b0001010000,
+
+    INPUT_VIN3_VIN2         = 0b0001100010,
+    INPUT_VIN3_VINCOM       = 0b0001110000,
+
+    INPUT_VIN4_VIN5         = 0b0010000101,
+    INPUT_VIN4_VINCOM       = 0b0010010000,
+
+    INPUT_VIN5_VIN4         = 0b0010100100,
+    INPUT_VIN5_VINCOM       = 0b0010110000,
+
+    INPUT_VIN6_VIN7         = 0b0011000111,
+    INPUT_VIN6_VINCOM       = 0b0011010000,
+
+    INPUT_VIN7_VIN6         = 0b0011100110,
+    INPUT_VIN7_VINCOM       = 0b0011110000,
+
+    INPUT_VIN8_VIN9         = 0b0100001001,
+    INPUT_VIN8_VINCOM       = 0b0100010000,
+
+    INPUT_VIN9_VIN8         = 0b0100101000,
+    INPUT_VIN9_VINCOM       = 0b0100110000,
+
+    INPUT_VIN10_VIN11       = 0b0101001011,
+    INPUT_VIN10_VINCOM      = 0b0101010000,
+
+    INPUT_VIN11_VIN10       = 0b0101101010,
+    INPUT_VIN11_VINCOM      = 0b0101110000,
+
+    INPUT_VIN12_VIN13       = 0b0110001101,
+    INPUT_VIN12_VINCOM      = 0b0110010000,
+
+    INPUT_VIN13_VIN12       = 0b0110101100,
+    INPUT_VIN13_VINCOM      = 0b0110110000,
+
+    INPUT_VIN14_VIN15       = 0b0111001111,
+    INPUT_VIN14_VINCOM      = 0b0111010000,
+
+    INPUT_VIN15_VIN14       = 0b0111101110,
+    INPUT_VIN15_VINCOM      = 0b0111110000,
+
+    INPUT_TEMPERATURE       = 0b1000110010,
+    INPUT_REFERENCE         = 0b1010110110,
+
+} AD4115_InputSelection_t;
 
 
+typedef enum
+{
+    UNIPOLAR        = 0x0000,
+    BIPOLAR         = 0x1000,
+	REFBUF_P        = 0x0800,      /**< bit 11 REFBUF+ This bit enables or disables the REF+ input buffer.*/
+    REFBUF_N        = 0x0400,      /**< bit 10 REFBUF- This bit enables or disables the REF- input buffer. */
+    /**
+     * bits 9..8 INBUF enables or disables input buffers:
+     * 00 = disable input buffers
+     * 01 = Reserved
+     * 10 = Reserved
+     * 11 = enable input buffers
+     */
+	INPUT_BUFFERS   = 0x0300,
+    /**
+     * bits 7..6 are reserved. Set to 0
+     */
+    /**
+     * bits	5..4 REF_SEL selects the reference source for ADC conversion.
+	   00 = External reference (REF+-).
+	   10 = Internal reference (2.5V). Must also be enabled in via ADCMODE.
+	   11 = AVDD - AVSS. Low voltage reference.
+     */
+
+	EXTERNAL_REF    = 0x0000,
+	INTERNAL_REF    = 0x0020,
+	ADD_AVSS_REF    = 0x0030,
+
+    RESERVED        = 0x0000,  /**< bits 3..0 are reserved. Set to 0*/
+
+} AD4115_SetupConfiguration_t;
+
+
+
+typedef union
+{
+    uint16_t value;
+    struct
+    {
+        uint16_t input_selection    : 10;   // Bits 9:0
+        uint16_t reserved           : 2;    // Bits 11:10
+        uint16_t setup_selection    : 3;    // Bits 14:12
+        uint16_t enabled            : 1;    // Bit 15
+    } bits;
+} AD4115_ChannelRegister_t;
+
+typedef struct
+{
+    uint8_t channel;
+    AD4115_ChannelRegister_t conf;
+} AD4115_ChannelConf_t;
+
+typedef struct
+{
+    uint8_t id;         /**< Identifiant de l'objet */
+    SPI_t spi;          /**< Configuration SPI */
+    AD4115_ChannelConf_t *conf;
+} AD4115_t;
+
+uint8_t AD4115_Init(AD4115_t *obj);
+
+uint8_t AD4115_CheckConnection(AD4115_t *obj);
 
 #endif  // PLIB_AD4115_H
